12baaefa0b5538012957401d848538f4087b0279
eDr0pko <124775632+eDr0pko@users.noreply.github.com>
2025-03-14 09:49:18 +0100

    Update request.php

--- site-web/request.php
+++ site-web/request.php
@@ -1,33 +1,52 @@
 <?php
-include 'db.php';
+    include 'db.php';
 
-$type = isset($_GET['type']) ? $_GET['type'] : '';
+    function load_users_data($conn){
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($conn, $sql);
 
-// Récupérer les artistes
-if ($type == 'artists') {
-  $sql = "SELECT username FROM artist JOIN users ON artist.mail = users.mail";
-  $result = $conn->query($sql);
-  $artists = [];
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $artists[] = $row;
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>";
+            echo "<thead><tr><th>Mail (PK)</th><th>Lastname</th><th>firstname</th><th>password</th><th>profile_picture</th></tr></thead>"; 
+            while ($row = mysqli_fetch_assoc($result)) { 
+                echo "<tr>";            
+                echo "<th>$row[mail]</th>";
+                echo "<td> $row[lastname] </td>";
+                echo "<td> $row[firstname] </td>";
+                echo "<td> $row[password] </td>";
+                echo "<td> $row[profile_picture] </td>";
+                echo "</tr>";
+            }
+        }
+        echo "</table>";
     }
-  }
-  echo json_encode($artists);
-}
 
-// Récupérer les chansons
-if ($type == 'songs') {
-  $sql = "SELECT name, time FROM songs";
-  $result = $conn->query($sql);
-  $songs = [];
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $songs[] = $row;
+    function load_songs_data($conn){
+        $sql = "SELECT * FROM songs";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>";
+            echo "<thead><tr><th>Id_song</th><th>name</th><th>song</th><th>picture</th><th>Id_artist</th></tr></thead>";
+            while ($row = mysqli_fetch_assoc($result)) {  
+                echo "<tr>";            
+                echo "<th scope=\"row\">$row[id_song]</th>";
+                echo "<td> $row[name] </td>";
+                echo "<td> $row[song] </td>";
+                echo "<td> $row[picture] </td>";
+                echo "<td> $row[id_artist] </td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
     }
-  }
-  echo json_encode($songs);
-}
 
-$conn->close();
+    
+
+    if (isset($_POST['action']) && $_POST['action'] == "load_users_data") {
+        load_users_data($conn);
+    }
+    if (isset($_POST['action']) && $_POST['action'] == "load_songs_data") {
+        load_songs_data($conn);
+    }
 ?>

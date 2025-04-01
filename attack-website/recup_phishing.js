0a51368fd72f2dd4dae92fa9584d18efe1ed9431
kant1 <124177119+MIleia@users.noreply.github.com>
2025-04-01 09:38:59 +0200

    Create recup_phishing.js

---
+++ attack-website/recup_phishing.js
@@ -0,0 +1,20 @@
+function chargerFichier() {
+    fetch("phishing.txt?" + new Date().getTime()) // Ajoute un timestamp pour éviter le cache
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("contenu").textContent = data;
+        })
+        .catch(error => {
+            document.getElementById("contenu").textContent = "Erreur lors du chargement.";
+            console.error("Erreur :", error);
+        });
+}
+
+
+// Charger le fichier immédiatement au démarrage
+chargerFichier();
+
+// Actualiser toutes les 1 secondes
+setInterval(chargerFichier, 1000);
+
+

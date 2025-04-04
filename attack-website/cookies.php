47adb9811a1b918d79d01a0a333d32b68f7bea82
eDr0pko <esteban.fanjul@isen-ouest.yncrea.fr>
2025-04-04 10:39:11 +0200

    push

--- attack-website/cookies.php
+++ attack-website/cookies.php
@@ -1,7 +1,11 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 
     ob_start();
 
